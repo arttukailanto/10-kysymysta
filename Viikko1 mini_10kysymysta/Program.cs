74195b87f46e0fdbffaf460a423ee92b32891918
MChorin <miika.chorin@gmail.com>
2018-09-19 14:19:27 +0300

    Add project files.

---
+++ Viikko1 mini_10kysymysta/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Viikko1_mini_10kysymysta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Kysymys kysymys = new Kysymys();
+            string vastaus = "";
+            int pisteet = 0;
+            
+
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(kysymys.KysyKysymys());
+                Console.Write("Vastaus: ");
+
+                vastaus = Console.ReadLine();
+
+                if ( vastaus == "k" || vastaus == "K")
+                {
+                    pisteet++;
+                    Console.WriteLine("Oikea vastaus!");
+                        
+                }
+                
+                else
+                {
+                    Console.WriteLine("Vastaus väärin :( ");
+                }
+               
+            }
+            Console.WriteLine("Pistemääräsi on: " + pisteet);
+
+            Console.ReadLine();
+
+        }
+
+    }
+}
